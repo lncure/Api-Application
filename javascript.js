d2c4faffc2b59725985af27ff3de64a96c7ce118
Victor Pharsen <vnlo.skp@edu.mercantec.dk>
2020-03-11 07:57:12 +0100

    Made the wheather api functional
    It still needs some tweaking to be perfect though i believe.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
-$.ajax({
-	type: "GET",
-	url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={94eb8f49abd52ce0348d1f50456d26a8}",
-	success: function(weatherData) {
+$(document).ready(function(){
+    $("#button").click(function(){
+        var city = $("#city").val();
+        console.log(city);
 
-	$("#name").html(weatherData.title);
-	$("#info").html(weatherData.explanation);
-	$("#apod").attr("src", weatherData.url)
-       }
-    })
-}
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=94eb8f49abd52ce0348d1f50456d26a8",
+            success: function(weatherData) {
+                console.log(weatherData);
+
+                var icon = "http://api.openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";
+                var temp = Math.floor(weatherData.list[0].main.temp) + "°F";
+                var weather = weatherData.list[0].weather[0].main;
+
+                $(".icon").attr('src', icon);
+                $(".temp").append(temp);
+                $(".weather").append(weather);
+            }
+        });  
+    });
+});
